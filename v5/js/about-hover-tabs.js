e75ec04cccf06dd200252e93b099d3893904108d
Paython <119300945+MyW0rld@users.noreply.github.com>
2025-02-15 18:24:39 +0100

    Add files via upload

---
+++ v5/js/about-hover-tabs.js
@@ -0,0 +1,20 @@
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active-butt"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active-butt", "");
+  }
+
+  // Show the current tab, and add an "active-butt" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active-butt";
+}
