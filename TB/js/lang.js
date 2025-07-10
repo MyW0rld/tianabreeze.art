67e04ff91aa9a58ea680875da15e7eea45a33bab
Paython <119300945+MyW0rld@users.noreply.github.com>
2025-07-10 21:55:59 +0200

    Update lang.js

--- TB/js/lang.js
+++ TB/js/lang.js
@@ -4,7 +4,8 @@ indexMeetTheArtist: "Meet The Artist",
 indexImTiana: "I'm Tania",
 indexQuote: `<br>A huge warm welcome to you!<br>I draw endless inspiration from traveling on a yacht and translate my love for the coast with impressionistic seascapes, images of boats, yachts and beach life. Art is my life and passion.`,
 indexLearnMoreButton: "Learn More",
-	  
+
+
     aboutMeTitle: "ABOUT ME",
     aboutParagraph1: `<br>My name is Tiana Breeze and I am a Belarusian-Polish artist, born in 1982 in Vitebsk, Belarus. My mother is Belarusian, and my father is Polish. We often went to Poland to visit my father's relatives. In 2017, my family and I moved to Poland for permanent residence, where I live and create to this day. 
 		<br></br>I have been drawing since childhood and initially became famous in my country as a caricaturist. Since the age of 13, my drawings have already been published in magazines and newspapers, such as "Vecherniy Vitebsk", Vitebsk Worker, Vitbichi, Picnic, Vozhyk, Shapoklyak and others.`,
@@ -14,14 +15,33 @@ indexLearnMoreButton: "Learn More",
         <br>Because of some rational reasons I changed my decision and became a manager  after two years at the Vitebsk State Technological University with a degree in manager-economist. Although my career was going well, I later realized that it was not what I really wanted.
 			Moving to Poland, my father's homeland, allowed me to continue painting. Now I am a professional full-time artist and I am happy to be able to do what I love.
 		</br><br>I am a member of the Union of Polish Artists, Painters and Graphic Artists. My artworks are featured in private collections in the USA, UK, Germany, Belarus, Poland and others.`,
-    
+    aboutParagraph3: `
+        The elemen t of water occupies a central place in my art.
+It has the power to refract light, blur boundaries, and create an illusion of reality that teeters on the edge of abstraction.
+Water is never the same — just like human emotions, memories, and experiences. It is this fluidity and impermanence that attract me, compelling me to search again and again for new ways to convey its living essence on canvas.
+
+I work with a multilayered oil painting technique, which allows me to achieve complex color nuances and depth.
+Reflections, optical effects, and the interaction of light with the surface of water become more than just subjects — they are means of expression. They allow me to go beyond literal depiction, creating a space filled with air, movement, and emotion.
+
+I am fascinated by how objects transform in water, dissolve, and take on new forms.
+It is a view of the world not directly, but through its reflection — a perspective that makes perception deeper, more intriguing, prompting the viewer to look closer and search for new meanings.
+
+Each of my works is the result of a journey — both external and internal.
+In my paintings, I capture not only a moment but also its emotional resonance, allowing the viewer to experience it and feel its uniqueness.
+
+For those who seek depth.
+For those who never stop.
+For those who want to feel.`,
+	  
     aboutArtTitle: "ABOUT MY ART",
     aboutQuote: `"Look at my pictures and just breath..."`,
 
+	  
 contactAnyQuestions: "Do you have any questions?",
 contactFeelFreeViaEmail: `Feel free to contact me on my email:  <a href="mailto:tianabreezeart@gmail.com" class="contact-email" style="letter-spacing: 2px;" target="_blank">tianabreezeart@gmail.com</a> <br>or on Contact Form.</br>`,
 contactJustFillOut: `Just fill out the form below to get started.`,
 contactPrivacyPolicy: `By clicking on the button, you consent to the processing of personal data and agree to the privacy policy`,
+
 	  
     footerText: "© Copyright Tiana Breeze Art",
     shipping: "SHIPPING & RETURNS",
@@ -33,14 +53,35 @@ contactPrivacyPolicy: `By clicking on the button, you consent to the processing 
 	indexImTiana: "Nazywam się Tania",
 	indexQuote: `<br>Serdecznie Cię witam!</br>Czerpię nieskończoną inspirację z podróży jachtem i przekładam moją miłość do wybrzeża na impresjonistyczne pejzaże morskie, obrazy łodzi, jachtów i życia na plaży. Sztuka to moje życie i pasja.`,
 	indexLearnMoreButton: "Czytać więcej",
+
 	  
     aboutMeTitle: "O MNIE",
     aboutParagraph1: `<br>Nazywam się Tiana Breeze i jestem białorusko-polską artystką, urodzoną w 1982 roku w Witebsku na Białorusi. Moja mama jest Białorusinką, a tata Polakiem. Często jeździliśmy do Polski odwiedzać krewnych taty. W 2017 roku moja rodzina i ja przeprowadziliśmy się na stałe do Polski, gdzie mieszkam i tworzę do dziś.<br></br>Rysuję od dziecka, początkowo zdobywając sławę w moim kraju jako karykaturzystka. Już od 13. roku życia moje rysunki były publikowane w czasopismach i gazetach, takich jak „Wieczorny Witebsk”, „Witebski Robotnik”, „Witbiczi”, „Piknik”, „Wozhyk”, „Szapoklak” i inne.`,
     aboutParagraph2: `Miałam pięć wystaw indywidualnych. Jestem laureatką regionalnego konkursu karykatur. Kocham satyrę i karykaturę, ale później moją prawdziwą pasją stał się obraz olejny.<br><br></br>Ukończyłam Wydział Sztuk Pięknych i Grafiki na Uniwersytecie Państwowym w Witebsku. Dyplom wykonałam w pracowni malarstwa olejnego pod kierunkiem prof. Anatolija Chmila i obroniłam go z wyróżnieniem. Malowanie olejne okazało się najlepszą techniką do wyrażania mojej pasji twórczej.<br>Z przyczyn racjonalnych zmieniłam decyzję i zostałam menedżerem po dwóch latach studiów na Witebskim Państwowym Uniwersytecie Technologicznym na kierunku menedżer-ekonomista. Mimo że kariera dobrze się rozwijała, później zrozumiałam, że to nie było to, czego naprawdę chciałam. Przeprowadzka do Polski, ojczyzny mojego ojca, pozwoliła mi kontynuować malarstwo. Teraz jestem zawodową artystką na pełen etat i cieszę się, że mogę robić to, co kocham.<br><br>Jestem członkinią Związku Polskich Artystów Plastyków. Moje prace znajdują się w prywatnych kolekcjach w USA, Wielkiej Brytanii, Niemczech, na Białorusi, w Polsce i innych krajach.`,
+aboutParagraph3: `
+    Żywioł wody zajmuje centralne miejsce w mojej sztuce.  
+Ma moc załamywania światła, zacierania granic i tworzenia iluzji rzeczywistości balansującej na granicy abstrakcji.  
+Woda nigdy nie jest taka sama — tak jak ludzkie emocje, wspomnienia i doświadczenia.  
+To właśnie ta płynność i nietrwałość przyciągają mnie, zmuszając do ciągłego poszukiwania nowych sposobów ukazania jej żywej istoty na płótnie.
+
+Pracuję techniką warstwowego malarstwa olejnego, która pozwala mi uzyskać złożone niuanse kolorystyczne i głębię.  
+Refleksy, efekty optyczne i interakcje światła z powierzchnią wody stają się czymś więcej niż tylko tematami — są środkami wyrazu.  
+Pozwalają mi wyjść poza dosłowne przedstawienie, tworząc przestrzeń wypełnioną powietrzem, ruchem i emocją.
+
+Fascynuje mnie, jak obiekty zmieniają się w wodzie, rozpuszczają się i przybierają nowe formy.  
+To spojrzenie na świat nie bezpośrednio, lecz przez jego odbicie — perspektywa, która pogłębia postrzeganie, intryguje i skłania widza do zatrzymania się i poszukiwania nowych znaczeń.
+
+Każda z moich prac to wynik podróży — zarówno zewnętrznej, jak i wewnętrznej.  
+Na moich obrazach utrwalam nie tylko chwilę, ale także jej emocjonalny rezonans, pozwalając widzowi doświadczyć jej i poczuć jej wyjątkowość.
+
+Dla tych, którzy szukają głębi.  
+Dla tych, którzy nigdy nie przestają.  
+Dla tych, którzy chcą poczuć.`,
 
     aboutArtTitle: "O MOJEJ SZTUCE",
     aboutQuote: `"Spójrz na moje obrazy i po prostu oddychaj..."`,
 
+	  
 contactAnyQuestions: "Czy masz jakieś pytania?",
 contactFeelFreeViaEmail: "",contactFeelFreeViaEmail: `Zapraszam do kontaktu przez e-mail: <a href="mailto:tianabreezeart@gmail.com" class="contact-email" style="letter-spacing: 2px;" target="_blank">tianabreezeart@gmail.com</a> <br>lub poprzez formularz kontaktowy.</br>`,
 contactJustFillOut: `Wypełnij poniższy formularz, aby rozpocząć.`,
