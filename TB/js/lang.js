fc74c6ddb00981aeba8232f8b4f282ec7d0e5fca
Paython <119300945+MyW0rld@users.noreply.github.com>
2025-07-10 15:49:58 +0200

    Update lang.js

--- TB/js/lang.js
+++ TB/js/lang.js
@@ -1,5 +1,10 @@
 const translations = {
   en: {
+indexMeetTheArtist: "Meet The Artist",
+indexImTiana: "I'm Tania",
+indexQuote: "<br>A huge warm welcome to you!</br>I draw endless inspiration from traveling on a yacht and translate my love for the coast with impressionistic seascapes, images of boats, yachts and beach life. Art is my life and passion.",
+indexLearnMoreButton: "Learn More",
+	  
     aboutMeTitle: "ABOUT ME",
     aboutParagraph1: `<br>My name is Tiana Breeze and I am a Belarusian-Polish artist, born in 1982 in Vitebsk, Belarus. My mother is Belarusian, and my father is Polish. We often went to Poland to visit my father's relatives. In 2017, my family and I moved to Poland for permanent residence, where I live and create to this day. 
 			<br></br>I have been drawing since childhood and initially became famous in my country as a caricaturist. Since the age of 13, my drawings have already been published in magazines and newspapers, such as "Vecherniy Vitebsk", Vitebsk Worker, Vitbichi, Picnic, Vozhyk, Shapoklyak and others.`,
@@ -20,6 +25,12 @@ const translations = {
     followMe: "Want to see behind the scenes? Follow me!",
   },
   pl: {
+
+	indexMeetTheArtist: "Poznaj Artystkę",
+	indexImTiana: "Nazywam się Tania",
+	indexQuote: `<br>Serdecznie Cię witam!</br>Czerpię nieskończoną inspirację z podróży jachtem i przekładam moją miłość do wybrzeża na impresjonistyczne pejzaże morskie, obrazy łodzi, jachtów i życia na plaży. Sztuka to moje życie i pasja.`,
+	indexLearnMoreButton: "Czytać więcej",
+	  
     aboutMeTitle: "O MNIE",
     aboutParagraph1: `<br>Nazywam się Tiana Breeze i jestem białorusko-polską artystką, urodzoną w 1982 roku w Witebsku na Białorusi. Moja mama jest Białorusinką, a tata Polakiem. Często jeździliśmy do Polski odwiedzać krewnych taty. W 2017 roku moja rodzina i ja przeprowadziliśmy się na stałe do Polski, gdzie mieszkam i tworzę do dziś.<br></br>Rysuję od dziecka, początkowo zdobywając sławę w moim kraju jako karykaturzystka. Już od 13. roku życia moje rysunki były publikowane w czasopismach i gazetach, takich jak „Wieczorny Witebsk”, „Witebski Robotnik”, „Witbiczi”, „Piknik”, „Wozhyk”, „Szapoklak” i inne.`,
     aboutParagraph2: `Miałam pięć wystaw indywidualnych. Jestem laureatką regionalnego konkursu karykatur. Kocham satyrę i karykaturę, ale później moją prawdziwą pasją stał się obraz olejny.<br><br></br>Ukończyłam Wydział Sztuk Pięknych i Grafiki na Uniwersytecie Państwowym w Witebsku. Dyplom wykonałam w pracowni malarstwa olejnego pod kierunkiem prof. Anatolija Chmila i obroniłam go z wyróżnieniem. Malowanie olejne okazało się najlepszą techniką do wyrażania mojej pasji twórczej.<br>Z przyczyn racjonalnych zmieniłam decyzję i zostałam menedżerem po dwóch latach studiów na Witebskim Państwowym Uniwersytecie Technologicznym na kierunku menedżer-ekonomista. Mimo że kariera dobrze się rozwijała, później zrozumiałam, że to nie było to, czego naprawdę chciałam. Przeprowadzka do Polski, ojczyzny mojego ojca, pozwoliła mi kontynuować malarstwo. Teraz jestem zawodową artystką na pełen etat i cieszę się, że mogę robić to, co kocham.<br><br>Jestem członkinią Związku Polskich Artystów Plastyków. Moje prace znajdują się w prywatnych kolekcjach w USA, Wielkiej Brytanii, Niemczech, na Białorusi, w Polsce i innych krajach.`,
@@ -28,22 +39,10 @@ const translations = {
     aboutParagraph2: "Sztuka to moje życie i pasja...",
     quote: `"Spójrz na moje obrazy i po prostu oddychaj..."`,
     tianaName: "Tiana Breeze",
+
     footerText: "© Prawa autorskie Tiana Breeze Art",
     shipping: "WYSYŁKA I ZWROTY",
     followMe: "Chcesz zobaczyć kulisy mojej pracy? Obserwuj mnie!",
-  },
-  fr: {
-    aboutMeTitle: "À PROPOS DE MOI",
-    aboutParagraph1: `<br>Je m'appelle Tiana Breeze et je suis une artiste biélorusse-polonaise, née en 1982 à Vitebsk, en Biélorussie. Ma mère est biélorusse et mon père est polonais. Nous allions souvent en Pologne rendre visite aux proches de mon père. En 2017, ma famille et moi avons déménagé en Pologne pour y vivre définitivement, où je vis et crée encore aujourd'hui.<br></br>Je dessine depuis mon enfance et je suis devenue célèbre dans mon pays en tant que caricaturiste. Dès l'âge de 13 ans, mes dessins ont été publiés dans des magazines et des journaux, tels que « Vecherniy Vitebsk », Vitebsk Worker, Vitbichi, Picnic, Vozhyk, Shapoklyak et d'autres.`,
-    aboutParagraph2: `J'ai organisé cinq expositions individuelles. Je suis lauréate d'un concours régional de dessins animés. J'adore la satire et la caricature, mais ma vraie passion est devenue la peinture à l'huile.<br><br></br>J'ai obtenu mon diplôme à la Faculté des Beaux-Arts et des Arts Graphiques de l'Université d'État de Vitebsk. J'ai réalisé mon travail de diplôme dans l'atelier de peinture à l'huile sous la direction du professeur Anatoly Chmil et je l'ai soutenu avec mention. J'ai trouvé que la peinture à l'huile était la meilleure technique pour exprimer ma passion créative.<br>Pour des raisons rationnelles, j'ai changé d'avis et suis devenue manager après deux ans à l'Université Technologique d'État de Vitebsk, diplômée en gestion-économie. Bien que ma carrière se développait bien, j'ai réalisé plus tard que ce n'était pas ce que je voulais vraiment. Déménager en Pologne, la patrie de mon père, m'a permis de continuer à peindre. Maintenant, je suis artiste professionnelle à plein temps et je suis heureuse de pouvoir faire ce que j'aime.<br><br>Je suis membre de l'Union des Artistes Polonais, Peintres et Graphistes. Mes œuvres figurent dans des collections privées aux États-Unis, au Royaume-Uni, en Allemagne, en Biélorussie, en Pologne et ailleurs.`,
- 
-    aboutArtTitle: "À PROPOS DE MON ART",
-    aboutParagraph2: "L'art est ma vie et ma passion...",
-    quote: `"Regardez mes tableaux et respirez simplement..."`,
-    tianaName: "Tiana Breeze",
-    footerText: "© Droits d'auteur Tiana Breeze Art",
-    shipping: "LIVRAISON ET RETOURS",
-    followMe: "Vous voulez voir les coulisses ? Suivez-moi !",
   }
 };
 
