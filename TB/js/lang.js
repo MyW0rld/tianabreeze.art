e920c786a7e451067ea88c99d4366cac0f94b73b
Paython <119300945+MyW0rld@users.noreply.github.com>
2025-07-10 16:28:52 +0200

    Update lang.js

--- TB/js/lang.js
+++ TB/js/lang.js
@@ -2,24 +2,22 @@ const translations = {
   en: {
 indexMeetTheArtist: "Meet The Artist",
 indexImTiana: "I'm Tania",
-indexQuote: "<br>A huge warm welcome to you!</br>I draw endless inspiration from traveling on a yacht and translate my love for the coast with impressionistic seascapes, images of boats, yachts and beach life. Art is my life and passion.",
+indexQuote: `<br>A huge warm welcome to you!<br>I draw endless inspiration from traveling on a yacht and translate my love for the coast with impressionistic seascapes, images of boats, yachts and beach life. Art is my life and passion.`,
 indexLearnMoreButton: "Learn More",
 	  
     aboutMeTitle: "ABOUT ME",
     aboutParagraph1: `<br>My name is Tiana Breeze and I am a Belarusian-Polish artist, born in 1982 in Vitebsk, Belarus. My mother is Belarusian, and my father is Polish. We often went to Poland to visit my father's relatives. In 2017, my family and I moved to Poland for permanent residence, where I live and create to this day. 
-			<br></br>I have been drawing since childhood and initially became famous in my country as a caricaturist. Since the age of 13, my drawings have already been published in magazines and newspapers, such as "Vecherniy Vitebsk", Vitebsk Worker, Vitbichi, Picnic, Vozhyk, Shapoklyak and others.`,
+		<br></br>I have been drawing since childhood and initially became famous in my country as a caricaturist. Since the age of 13, my drawings have already been published in magazines and newspapers, such as "Vecherniy Vitebsk", Vitebsk Worker, Vitbichi, Picnic, Vozhyk, Shapoklyak and others.`,
     aboutParagraph2: `I have held five solo exhibitions. I am a winner of the regional cartoon competition. I love satire and caricatures, but later my real passion became oil painting.
-			<br><br></br>I graduated from the Faculty of Fine Arts and Graphics at the State University in Vitebsk. I completed my diploma artwork in the oil painting studio under the guidance of prof. Anatoly Chmil and defended it with honors. I found oil painting to be the best technique to channel my creative passion. 
-		</br>
+			<br><br></br>I graduated from the Faculty of Fine Arts and Graphics at the State University in Vitebsk. I completed my diploma artwork in the oil painting studio under the guidance of prof. Anatoly Chmil and defended it with honors. I found oil painting to be the best technique to channel my creative passion.</br>
         
         <br>Because of some rational reasons I changed my decision and became a manager  after two years at the Vitebsk State Technological University with a degree in manager-economist. Although my career was going well, I later realized that it was not what I really wanted.
 			Moving to Poland, my father's homeland, allowed me to continue painting. Now I am a professional full-time artist and I am happy to be able to do what I love.
 		</br><br>I am a member of the Union of Polish Artists, Painters and Graphic Artists. My artworks are featured in private collections in the USA, UK, Germany, Belarus, Poland and others.`,
     
     aboutArtTitle: "ABOUT MY ART",
-    aboutParagraph2: "Art is my life and passion...",
-    quote: `"Look at my pictures and just breath..."`,
-    tianaName: "Tiana Breeze",
+    aboutQuote: `"Look at my pictures and just breath..."`,
+
     footerText: "© Copyright Tiana Breeze Art",
     shipping: "SHIPPING & RETURNS",
     followMe: "Want to see behind the scenes? Follow me!",
@@ -36,9 +34,7 @@ indexLearnMoreButton: "Learn More",
     aboutParagraph2: `Miałam pięć wystaw indywidualnych. Jestem laureatką regionalnego konkursu karykatur. Kocham satyrę i karykaturę, ale później moją prawdziwą pasją stał się obraz olejny.<br><br></br>Ukończyłam Wydział Sztuk Pięknych i Grafiki na Uniwersytecie Państwowym w Witebsku. Dyplom wykonałam w pracowni malarstwa olejnego pod kierunkiem prof. Anatolija Chmila i obroniłam go z wyróżnieniem. Malowanie olejne okazało się najlepszą techniką do wyrażania mojej pasji twórczej.<br>Z przyczyn racjonalnych zmieniłam decyzję i zostałam menedżerem po dwóch latach studiów na Witebskim Państwowym Uniwersytecie Technologicznym na kierunku menedżer-ekonomista. Mimo że kariera dobrze się rozwijała, później zrozumiałam, że to nie było to, czego naprawdę chciałam. Przeprowadzka do Polski, ojczyzny mojego ojca, pozwoliła mi kontynuować malarstwo. Teraz jestem zawodową artystką na pełen etat i cieszę się, że mogę robić to, co kocham.<br><br>Jestem członkinią Związku Polskich Artystów Plastyków. Moje prace znajdują się w prywatnych kolekcjach w USA, Wielkiej Brytanii, Niemczech, na Białorusi, w Polsce i innych krajach.`,
 
     aboutArtTitle: "O MOJEJ SZTUCE",
-    aboutParagraph2: "Sztuka to moje życie i pasja...",
-    quote: `"Spójrz na moje obrazy i po prostu oddychaj..."`,
-    tianaName: "Tiana Breeze",
+    aboutQuote: `"Spójrz na moje obrazy i po prostu oddychaj..."`,
 
     footerText: "© Prawa autorskie Tiana Breeze Art",
     shipping: "WYSYŁKA I ZWROTY",
